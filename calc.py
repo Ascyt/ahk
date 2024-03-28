2489a324da8b1fc5bfb8b9e05022a05c05f48602
Ascyt <filip.schauer07@gmail.com>
2024-03-28 20:42:41 +0100

    Added ; to calc.py to execute multiple statements

--- calc.py
+++ calc.py
@@ -2,7 +2,18 @@ from math import *
 import os
 
 arg = ' '.join(os.sys.argv[1:]) if len(os.sys.argv) > 1 else input('> ')
+arg = arg.replace('^', '**')
 
 with open('calc_output.txt', 'w') as f:
-    output = eval(arg)
-    f.write(str(output))
+    f.write("ERROR")
+
+args = arg.split(';')
+
+current = None
+
+for arg in args:
+    current_arg = str(current) + arg if current != None else arg
+    current = eval(current_arg)
+
+with open('calc_output.txt', 'w') as f:
+    f.write(str(current))
